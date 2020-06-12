c03f53968834ec9f137e0d59a341aa986acfd19f
Abhishek Jangra <iabhishek1041@gmail.com>
2020-06-12 21:19:21 +0530

    [clojure] get final solution for set 1 challenge 1

--- clojure/S1C1.clj
+++ clojure/S1C1.clj
@@ -41,6 +41,22 @@
   [sequenceOfBinaryStrings]
   (map #(getDecimalValueForBinaryString %) sequenceOfBinaryStrings))
 
+(defn getHashMapForDecimalToBase64Conversion
+  []
+  [\A, \B, \C, \D, \E, \F, \G, \H, \I, \J, \K, \L, \M
+   \N, \O, \P, \Q, \R, \S, \T, \U, \V, \W, \X, \Y, \Z
+   \a, \b, \c, \d, \e, \f, \g, \h, \i, \j, \k, \l, \m
+   \n, \o, \p, \q, \r, \s, \t, \u, \v, \w, \x, \y, \z
+   \0, \1, \2, \3, \4, \5, \6, \7, \8, \9, \+, \/])
+
+(defn getBase64StringFromDecimalSequence
+  [decimalSequence]
+  (reduce str (map #(get (getHashMapForDecimalToBase64Conversion) %) decimalSequence)))
+
 (defn getBase64StringForHex
   [hexString]
-  (getSequenceOfDecimalValuesFromBinary (getSequenceOf6CharStringsFromLengthyString (getPaddedBinaryStringForBase64Conversion (getBinaryStringForHexString hexString)))))
+  (getBase64StringFromDecimalSequence (getSequenceOfDecimalValuesFromBinary (getSequenceOf6CharStringsFromLengthyString (getPaddedBinaryStringForBase64Conversion (getBinaryStringForHexString hexString))))))
+
+(defn getSolutionToS1C1
+  []
+  (getBase64StringForHex "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
